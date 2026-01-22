67d296133b32075afaac20dd6c50f6ed21545273
Hazrat Ali <163515344+hazratali1@users.noreply.github.com>
2026-01-22 20:02:20 +0600

    Update vector_store.py

--- rag_chatbot/app/vector_store.py
+++ rag_chatbot/app/vector_store.py
@@ -1,17 +1,15 @@
-#Vector search
+# Creates vector database
 
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 
 def create_vector_store(chunks):
-    embeddings = HuggingFaceEmbeddings(
+    embedding = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    vectorstore = FAISS.from_documents(
+    return FAISS.from_documents(
         documents=chunks,
-        embedding=embeddings
+        embedding=embedding
     )
-
-    return vectorstore
